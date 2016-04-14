cdd501b97f96f693031ab22cfcd04e5b75a33ede
pavel <slashers0017@gmail.com>
2016-04-14 22:30:28 +0300

    trial

---
+++ barrier.h
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <mutex>
+#include <condition_variable>
+
+class barrier
+{
+public:
+	explicit barrier(size_t threadsNum): capacity(threadsNum), count(0), door(false) {}
+	void enter();
+private:
+	size_t capacity;
+	size_t count;
+	std::condition_variable barrier1, barrier2;
+	std::mutex mut;
+	bool door;
+};
+
+void barrier::enter()
+{
+	std::unique_lock<std::mutex> lock(mut);
+	// ждем, пока наберется capacity потоков
+	barrier1.wait(lock, [this] () {return !door;} );
+	++count;
+	barrier2.wait(lock, [this] () {return capacity == count || door;} );
+	// последний пришедший открывает всем дверь и они заходят
+	door = true;
+	--count;
+	barrier2.notify_all();
+	// последний вошедший закрывает дверь и все ждущие за дверью делают то же заново
+	if (count == 0)
+	{
+        door = false;
+		barrier1.notify_all();
+	}
+}
+
---
+++ blocking_queue.h
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <queue>
+#include <thread>
+#include <condition_variable>
+#include <mutex>
+
+template <class Value, class Container = std::deque<Value> >
+class thread_safe_queue
+{
+public:
+    thread_safe_queue(size_t capacity_): isClosed(false), capacity(capacity_) {}
+    void enqueue(Value item);
+    void pop(Value& item);
+    void shutdown();
+private:
+    bool isClosed;
+    Container queue;
+    size_t capacity;
+    std::condition_variable isFull;
+    std::condition_variable isEmpty;
+    std::mutex mut;
+};
+
+class enqException : public std::exception
+{
+    virtual const char* what() const throw()
+    {
+        return "Push after shutdown.";
+    }
+};
+
+class deqException : public std::exception
+{
+    virtual const char* what() const throw()
+    {
+        return "Pop from empty queue after shutdown.";
+    }
+};
+template <class Value, class Container>
+void thread_safe_queue<Value, Container>::enqueue(Value item)
+{
+    std::unique_lock<std::mutex> lock(mut);
+    isFull.wait(lock, [this] () {return queue.size() != capacity || isClosed;});
+    if (isClosed)
+        throw enqException();
+    queue.push_back(move(item));
+    isEmpty.notify_one();
+}
+
+template <class Value, class Container>
+void thread_safe_queue<Value, Container>::pop(Value& item)
+{
+    std::unique_lock<std::mutex> lock(mut);
+    isEmpty.wait(lock, [this] () {return !queue.empty() || isClosed;});
+    if (queue.empty())
+        throw deqException();
+    item = std::move(queue.front());
+    queue.pop_front();
+    isFull.notify_one();
+}
+
+template <class Value, class Container>
+void thread_safe_queue<Value, Container>::shutdown()
+{
+    isClosed = true;
+    isEmpty.notify_all();
+    isFull.notify_all();
+}
